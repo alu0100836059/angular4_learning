b23d680e163553b5da00934178cd9e83064eef9e
alu0100836059 <alu0100836059@ull.edu.es>
2017-10-23 18:24:18 +0200

    Ready the routing demo, beginning Data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
+import { ProductsComponent } from './products/products.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent
+    MyComponentComponent,
+    ProductsComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'members',
+        component: MembersComponent
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
